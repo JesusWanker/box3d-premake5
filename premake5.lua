f440f58ddb1eb6c1bf892d68ef99de35a8bc7d00
Jesus Wanker <WillGamingQc@hotmail.com>
2026-07-04 14:27:12 -0400

    Create premake5.lua
    Added premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,38 @@
+
+
+project "box3d"
+  kind "StaticLib" --Can be replaced to "SharedLib" for a DLL
+  language "C++"
+
+  cdialect "C11"
+  cppdialect "C++17"
+
+  architecture "x86_64"
+
+  targetdir ("%{wks.location}/bin/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}/%{prj.name}")
+  objdir ("%{wks.location}/bin-int/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}/%{prj.name}")
+
+  includedirs {
+    "include",
+    "src"
+  }
+
+files {
+  "include/**.h"
+  "src/**.c"
+  "src/**.cpp"
+  "src/**.h"
+}
+
+  filter "system:linux"
+    buildoptions { "-ffp-contract=off" }
+
+  filter "system:windows"
+    systemversion "latest"
+    staticruntime "On" -- Set to Off if MSVC MT/MTd or On if MSVC MD/MDd
+
+filter "configurations:Debug"
+  symbols "On"
+
+filter "configurations:Release"
+  symbols "Off"
